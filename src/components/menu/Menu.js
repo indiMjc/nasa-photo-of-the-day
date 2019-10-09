40861d7448e24e4f8f5b78bf3f7eab3303e50a48
indiMjc <45151346+indiMjc@users.noreply.github.com>
2019-10-09 17:55:00 -0400

    menu started

---
+++ src/components/menu/Menu.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Menu = () => {
+  return <span id="menu">Menu</span>;
+};
+
+export default Menu;
--- src/components/pageHeader/PageHeader.js
+++ src/components/pageHeader/PageHeader.js
@@ -1,7 +1,13 @@
 import React from "react";
+import Menu from "../menu/Menu";
 
 const PageHeader = props => {
-  return <h3 className="App-header">Image for {props.date}</h3>;
+  return (
+    <div className="App-header">
+      <Menu />
+      <h3>Image for {props.date}</h3>
+    </div>
+  );
 };
 
 export default PageHeader;
